bba92f25d32b6ff11f9f4e70972b55ce824414b9
Lydia <lychicherova@gmail.com>
2021-02-09 10:59:21 -0500

    add postal code column and data based on lan\lat from google map

--- express_backend/db/migrations/seeds.sql
+++ express_backend/db/migrations/seeds.sql
@@ -1,30 +1,33 @@
-INSERT INTO customers(id, name, address, phone, email, password, address_latitude,address_longitude) VALUES
- (1, 'Elen', 'Cortana Ave', '6134440099', 'elenaCor@mail.com', '1234', 45.449493, -75.637611);
-INSERT INTO customers(id, name, address, phone, email, password, address_latitude,address_longitude) VALUES
- (2, 'Ann', 'Guess Ave', '6134440022', 'AnnGues@mail.com', '4444', 45.449493, -75.737611);
-INSERT INTO customers(id, name, address, phone, email, password, address_latitude,address_longitude) VALUES
- (3, 'Roze', 'Lablaws Ave', '6134440088', 'RozeLab@mail.com', '1777', 45.549493, -75.637611);
-INSERT INTO customers(id, name, address, phone, email, password, address_latitude,address_longitude) VALUES
- (4, 'Zahra', 'Alice Str', '6134440011', 'ZahraAlc@mail.com', '0815', 45.349493, -75.437611);
-INSERT INTO customers(id, name, address, phone, email, password, address_latitude,address_longitude) VALUES
- (5, 'Martin', 'Cheers Ave', '6134441111', 'MartinCh@mail.com', '6111', 44.449493, -76.637611);
+INSERT INTO customers(id, name, address, phone, email, password, address_latitude,address_longitude, postal_code) VALUES
+ (1, 'Elen', 'Cortana Ave', '6134440099', 'elenaCor@mail.com', '1234', 45.449493, -75.637611, 'K1K4Z1');
+INSERT INTO customers(id, name, address, phone, email, password, address_latitude,address_longitude, postal_code) VALUES
+ (2, 'Ann', 'Guess Ave', '6134440022', 'AnnGues@mail.com', '4444', 45.449493, -75.737611, 'J8Y1N3 ');
+INSERT INTO customers(id, name, address, phone, email, password, address_latitude,address_longitude, postal_code) VALUES
+ (3, 'Roze', 'Lablaws Ave', '6134440088', 'RozeLab@mail.com', '1777', 45.549493, -75.637611,'J8R3G1');
+INSERT INTO customers(id, name, address, phone, email, password, address_latitude,address_longitude, postal_code) VALUES
+ (4, 'Zahra', 'Alice Str', '6134440011', 'ZahraAlc@mail.com', '0815', 45.349493, -75.437611, 'K4C9Z9');
+INSERT INTO customers(id, name, address, phone, email, password, address_latitude,address_longitude, postal_code) VALUES
+ (5, 'Martin', 'Cheers Ave', '6134441111', 'MartinCh@mail.com', '6111', 44.449493, -76.637611, 'K0H1V0');
 SELECT nextval('customers_id_seq'::regclass);
+
 ALTER SEQUENCE customers_id_seq RESTART WITH 6;
-INSERT INTO caterers (id, first_name, last_name, address, phone, email, password, account_number, shop_name, shop_logo, address_latitude,address_longitude) VALUES
-(1, 'Lawrence', 'Axe', 'Baltimore St', '6130006688', 'LawBalt@mail.com','1111', '123','happy shop', 'Image1',47.449493, -74.637611);
-INSERT INTO caterers (id, first_name, last_name, address, phone, email, password, account_number, shop_name, shop_logo, address_latitude,address_longitude) VALUES
-(2, 'Deen','Winchester', 'Grace St', '6130009999', 'DeenGrace@mail.com','2222','444','nice shop', 'Image2',47.449493, -76.637611);
-INSERT INTO caterers (id, first_name, last_name, address, phone, email, password, account_number, shop_name, shop_logo, address_latitude,address_longitude) VALUES
-(3, 'Marie','Joy', 'Van St', '6130001111', 'MarieVan@mail.com','3333', '333','hello shop', 'Image3',46.449493, -76.637611);
-INSERT INTO caterers (id, first_name, last_name, address, phone, email, password, account_number, shop_name, shop_logo, address_latitude,address_longitude) VALUES
-(4, 'Lara', 'Croft','Green St', '6130002233', 'LaraGreen@mail.com','4444', '578', 'grocery shop', 'Image1',47.449493, -75.937611);
+INSERT INTO caterers (id, first_name, last_name, address, phone, email, password, account_number, shop_name, shop_logo, address_latitude,address_longitude, postal_code) VALUES
+(1, 'Lawrence', 'Axe', 'Baltimore St', '6130006688', 'LawBalt@mail.com','1111', '123','happy shop', 'Image1',46.449493, -75.637611, 'J9L0T9');
+INSERT INTO caterers (id, first_name, last_name, address, phone, email, password, account_number, shop_name, shop_logo, address_latitude,address_longitude, postal_code) VALUES
+(2, 'Deen','Winchester', 'Grace St', '6130009999', 'DeenGrace@mail.com','2222','444','nice shop', 'Image2', 45.149493, -76.637611,'K0A2L0 ');
+INSERT INTO caterers (id, first_name, last_name, address, phone, email, password, account_number, shop_name, shop_logo, address_latitude,address_longitude, postal_code) VALUES
+(3, 'Marie','Joy', 'Van St', '6130001111', 'MarieVan@mail.com','3333', '333','hello shop', 'Image3', 45.281528, -75.876250, 'K2S0T7');
+INSERT INTO caterers (id, first_name, last_name, address, phone, email, password, account_number, shop_name, shop_logo, address_latitude,address_longitude, postal_code) VALUES
+(4, 'Lara', 'Croft','Green St', '6130002233', 'LaraGreen@mail.com','4444', '578', 'grocery shop', 'Image1', 45.400691, -75.620150, 'K1B4K1');
 SELECT nextval('caterers_id_seq'::regclass);
 ALTER SEQUENCE caterers_id_seq RESTART WITH 5;
+
 INSERT INTO statuses (id, name) VALUES (1,'in progres');
 INSERT INTO statuses (id, name) VALUES (2,'ready for pick up');
 INSERT INTO statuses (id, name) VALUES (3,'done');
 SELECT nextval('statuses_id_seq'::regclass);
 ALTER SEQUENCE statuses_id_seq RESTART WITH 4;
+
 INSERT INTO food_types (id,name, icon) VALUES (1, 'canadian', 'Image2');
 INSERT INTO food_types (id,name, icon) VALUES (2, 'oriental', 'Image3');
 INSERT INTO food_types (id,name, icon) VALUES (3, 'asian', 'Image2');
